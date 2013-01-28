d18b13728749ca8c0f208df945f59a8fe58d670b
Tobal San <tobalsan@gmail.com>
2013-01-28 03:14:44 +0100

    added UrlGenerator service provider

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,9 +10,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-// Register ServiceController service provider to use controllers as service
-$app->register(new Silex\Provider\ServiceControllerServiceProvider());
-
-// Twig templates
-$app->register(new Silex\Provider\TwigServiceProvider());
+$app->register(new Silex\Provider\ServiceControllerServiceProvider()); // Register ServiceController service provider to use controllers as service
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider()); // Twig templates
 $app['twig']->addExtension(new \Entea\Twig\Extension\AssetExtension($app));
