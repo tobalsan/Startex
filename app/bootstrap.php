ef2caaff40ede1c53fbae73c38e5eb1ed154fd3f
Tobal San <tobalsan@gmail.com>
2013-01-28 03:59:14 +0100

    added config and DBAL service providers

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,7 +10,24 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+// Config
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/parameters.yml"));de
+
+// DBAL Layer
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => $app['db']['driver'],
+        'host'     => $app['db']['host'],
+        'dbname'   => $app['db']['dbname'],
+        'user'     => $app['db']['user'],
+        'password' => $app['db']['password'],
+    ),
+));
+
 $app->register(new Silex\Provider\ServiceControllerServiceProvider()); // Register ServiceController service provider to use controllers as service
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider()); // Twig templates
 $app['twig']->addExtension(new \Entea\Twig\Extension\AssetExtension($app));
+
+// Model managers
+$app['some_service'] = $app->share(function ($app) { return new SmartRock\MainBundle\Model\JmoopaManager($app['db']); });
